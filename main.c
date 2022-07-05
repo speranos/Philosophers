50a381471bc6a5c8b49fa2860161d9784ad1afd7
Amine Oueldmama <aoueldma@e2r4p12.1337.ma>
2022-07-05 05:16:59 +0100

    enima

---
+++ main.c
@@ -0,0 +1,16 @@
+ft_arg_check(int ac, char **av)
+{
+	if (ac == 4 || ac == 5)
+	{
+
+	}
+	else
+	{	prin
+		return (1);
+}
+
+int	main(int ac, char **av)
+{
+	if (ft_arg_check(ac, av) == 1)
+		return (0);
+}
---
+++ philo.h
@@ -0,0 +1,7 @@
+#ifndef
+# define
+
+# include <stdio.h>
+# include <stdlib.h>
+
+#endif
