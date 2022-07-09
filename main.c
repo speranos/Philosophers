b4b65d3ace318b1fdad303303a164b89daa1e1b7
enimv <oueldmamaamine@gmail.com>
2022-07-09 04:45:51 +0100

    enima

--- main.c
+++ main.c
@@ -1,12 +1,37 @@
-ft_arg_check(int ac, char **av)
-{
-	if (ac == 4 || ac == 5)
-	{
+#include "philo.h"
 
-	}
-	else
-	{	prin
+int	ft_check_value(int ac, char **av)
+{
+	if (atoi(av[1]) <= 0)
 		return (1);
+	else if (atoi(av[2]) < 0)
+		return (1);
+	else if (atoi(av[3]) < 0)
+		return (1);
+	else if (atoi(av[4]) < 0)
+		return (1);
+	if (ac == 6)
+	{
+		if (atoi(av[5]) < 0)
+			return (1);
+	}
+	return (0);
+}
+
+int	ft_arg_check(int ac, char **av)
+{
+	(void)av;
+	if (ac < 5 || ac > 6)
+	{
+		printf("args...error !!!");
+		return (1);
+	}
+	if (ft_check_value(ac, av) == 1)
+	{
+		printf ("args...value...error!!!");
+		return (1);
+	}
+	return (0);
 }
 
 int	main(int ac, char **av)
--- philo.h
+++ philo.h
@@ -1,5 +1,5 @@
-#ifndef
-# define
+#ifndef PHILO_H
+# define PHILO_H
 
 # include <stdio.h>
 # include <stdlib.h>
