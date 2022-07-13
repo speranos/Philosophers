1b909ba34c5a2a67aeba80bfb982bd61ac49eee5
enimv <oueldmamaamine@gmail.com>
2022-07-13 15:38:19 +0100

    enima

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
 #include "philo.h"
 
+int	ft_check_type(int ac, char **av)
+{
+	int	i;
+
+	i = 0;
+	ac--;
+	while (ac > 1)
+	{
+		i = 0;
+		while (av[ac][i])
+		{
+			if(av[ac][i] < 48 || av[ac][i] >  57)
+				return (1);
+			i++;
+		}
+		ac--;
+	}
+	return (0);
+	
+}
+
 int	ft_check_value(int ac, char **av)
 {
 	if (atoi(av[1]) <= 0)
@@ -26,9 +47,14 @@ int	ft_arg_check(int ac, char **av)
 		printf("args...error !!!");
 		return (1);
 	}
+	if (ft_check_type(ac,av))
+	{
+		printf ("error...only number !!!");
+		return (1);
+	}
 	if (ft_check_value(ac, av) == 1)
 	{
-		printf ("args...value...error!!!");
+		printf ("args...value...error !!!");
 		return (1);
 	}
 	return (0);
@@ -38,4 +64,5 @@ int	main(int ac, char **av)
 {
 	if (ft_arg_check(ac, av) == 1)
 		return (0);
+	printf ("all good\n");
 }
